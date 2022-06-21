4c0cbe6292863c005d8c2902defc57737a4f0a56
Francisco Pladano <fpladano@Franciscos-MacBook-Pro.local>
2022-06-21 09:00:08 -0600

    Test

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Calculator from './components/Calculator';
 
 function App() {
   const [count, setCount] = useState(0);
-
+  // Test push on work mac
   return (
     <div className="flex items-center justify-center min-h-screen min-w-screen">
       <Calculator />
